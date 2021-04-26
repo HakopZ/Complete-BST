3c2d9c117c65ee529d7d95bc5412b137d007e5ce
HakopZarikyan <hakop2006@gmail.com>
2021-04-25 17:26:46 -0700

    Add project files.

---
+++ Complete BST/BST.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Complete_BST
+{
+    public class BST<T>
+    {
+        BST<T> Root;
+        T Value { get; }
+        
+        BST<T> Left { get; set; }
+        BST<T> Right { get; set; }
+
+        public int Count { get; private set; }
+        BST(T val)
+        {
+            Value = val;
+        }
+        BST(BST<T> values)
+        {
+            Root = values;
+        }
+        BST(T[] values)
+        {
+            foreach(var val in values)
+            {
+                Insert(val);
+            }
+        }
+        public BST<T> Insert(T value)
+        {
+            if(Root == null)
+            {
+                Root = new BST<T>(value);
+            }
+            return add(Root, value);
+        }
+        public BST<T> add(BST<T> curr, T value)
+        {
+            if (curr.Left == null)
+            {
+                curr.Left = new BST<T>(value);
+                return curr;
+            }
+            if (curr.Right == null)
+            {
+                curr.Right = new BST<T>(value);
+                return curr;
+            }
+            return add(curr.Left, value);
+        }
+    }
+
+}
